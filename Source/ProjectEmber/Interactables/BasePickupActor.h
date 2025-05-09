3c4eee1955dd31c0e42cb980df6bede2fc682725
HyeonjungYun <guswnd9971@gmail.com>
2025-05-10 05:26:19 +0900

    Update 채굴/채집 상호작용 애니메이션 추가, 픽업 이벤트 자연스럽게 수정
    코드 수정 필요

---
+++ Source/ProjectEmber/Interactables/BasePickupActor.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Interactables/BaseInteractableActor.h"
+#include "BasePickupActor.generated.h"
+
+UCLASS()
+class PROJECTEMBER_API ABasePickupActor : public ABaseInteractableActor
+{
+	GENERATED_BODY()
+
+public:
+	ABasePickupActor();
+	virtual void Interact_Implementation(AActor* Caller) override;
+	virtual void BeginPlay() override;
+
+protected:
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* MeshComp;
+	
+};
