239659dd57eca3eb33a9f04860376b93dc2b89f1
Cat Yubi <kimtaewan144@gmail.com>
2025-05-11 00:51:45 +0900

    Add Plugins, UI Architecture

---
+++ Source/ProjectEmber/UI/BaseWidget/EmberBaseWidget.cpp
@@ -0,0 +1,18 @@
+﻿#include "EmberBaseWidget.h"
+
+void UEmberBaseWidget::Show()
+{
+	PlayAnimation(OpenAnim);
+	OnOpened();
+}
+
+void UEmberBaseWidget::Hide()
+{
+	PlayAnimation(CloseAnim);
+	OnClosed();
+}
+
+void UEmberBaseWidget::Toggle()
+{
+	bIsVisible ? Hide() : Show();
+}
