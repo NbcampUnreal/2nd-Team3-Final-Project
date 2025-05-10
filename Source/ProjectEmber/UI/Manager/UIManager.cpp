72773fa84ebe222580cca4f3a83dfb90716c7394
Cat Yubi <kimtaewan144@gmail.com>
2025-05-11 00:51:45 +0900

    Add Plugins, UI Architecture

---
+++ Source/ProjectEmber/UI/Manager/UIManager.cpp
@@ -0,0 +1,59 @@
+﻿#include "UIManager.h"
+#include "UI/BaseWidget/EmberBaseWidget.h"
+
+void UUIManager::Initialize(FSubsystemCollectionBase& Collection)
+{
+	RegisterLayer(ELayer::Game,     WidgetClasses.FindChecked(ELayer::Game));
+	RegisterLayer(ELayer::GameMenu, WidgetClasses.FindChecked(ELayer::GameMenu));
+	RegisterLayer(ELayer::Menu,     WidgetClasses.FindChecked(ELayer::Menu));
+	RegisterLayer(ELayer::Modal,    WidgetClasses.FindChecked(ELayer::Modal));
+}
+
+void UUIManager::RegisterLayer(const ELayer Layer, const TSubclassOf<UEmberBaseWidget>& WidgetClass)
+{
+	WidgetClasses.Add(Layer, WidgetClass);
+}
+
+void UUIManager::Open(const ELayer Layer)
+{
+	if (UEmberBaseWidget* Widget = GetOrCreate(Layer))
+	{
+		Widget->Show();
+	}
+}
+
+void UUIManager::Close(const ELayer Layer)
+{
+	if (UEmberBaseWidget* Widget = GetOrCreate(Layer))
+	{
+		Widget->Hide();
+	}
+}
+
+void UUIManager::Toggle(const ELayer Layer)
+{
+	if (UEmberBaseWidget* Widget = GetOrCreate(Layer))
+	{
+		Widget->Toggle();
+	}
+}
+
+UEmberBaseWidget* UUIManager::GetOrCreate(ELayer Layer)
+{
+	if (Widgets.Contains(Layer))
+	{
+		return Widgets[Layer];
+	}
+	
+	if (const TSubclassOf<UEmberBaseWidget> WidgetClass = WidgetClasses.FindRef(Layer))
+	{
+		if (UEmberBaseWidget* Widget = CreateWidget<UEmberBaseWidget>(GetWorld(), WidgetClass))
+		{
+			Widget->AddToViewport(static_cast<int32>(Layer) * 100);
+			Widgets.Add(Layer, Widget);
+			return Widget;
+		}
+	}
+	return nullptr;
+}
+
---
+++ Source/ProjectEmber/UI/Manager/UIManager.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "UIManager.generated.h"
+
+class FSubsystemCollectionBase;
+enum class ELayer : FPlatformTypes::uint8;
+
+UCLASS()
+class PROJECTEMBER_API UUIManager : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	void RegisterLayer(ELayer Layer, const TSubclassOf<class UEmberBaseWidget>& WidgetClass);
+
+	void Open(ELayer Layer);
+	void Close(ELayer Layer);
+	void Toggle(ELayer Layer);
+
+private:
+	UEmberBaseWidget* GetOrCreate(ELayer Layer);
+
+	TMap<ELayer, TSubclassOf<UEmberBaseWidget>> WidgetClasses;
+	TMap<ELayer, TObjectPtr<UEmberBaseWidget>>  Widgets;
+};
