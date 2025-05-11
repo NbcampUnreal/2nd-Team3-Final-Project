30d6e91f1cfc36cffe576e692ec129b3b078afe5
Cat Yubi <kimtaewan144@gmail.com>
2025-05-11 17:24:38 +0900

    Add Cursor

---
+++ Source/ProjectEmber/UI/Manager/UIManagerHelper.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "UIManager.h"
+#include "UIManagerHelper.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTEMBER_API UUIManagerHelper : public UObject
+{
+	GENERATED_BODY()
+	
+public:
+	TMap<ELayer, TSubclassOf<UEmberBaseWidget>> WidgetClasses;
+};
