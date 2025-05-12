f2e5a76e14435da4ec01bee1e14f34050d7f702a
Cat Yubi <kimtaewan144@gmail.com>
2025-05-12 16:42:42 +0900

    Add UI

--- Source/ProjectEmber/UI/FunctionLibrary/UIFunctionLibrary.cpp
+++ Source/ProjectEmber/UI/FunctionLibrary/UIFunctionLibrary.cpp
@@ -1,10 +1,12 @@
 ﻿#include "UIFunctionLibrary.h"
 #include "GameplayTagContainer.h"
+#include "Blueprint/WidgetBlueprintLibrary.h"
 #include "UI/HUD/EmberMainHUD.h"
+#include "Kismet/GameplayStatics.h"
 
 bool UUIFunctionLibrary::RegisterLayer(APlayerController* OwningPlayer, FGameplayTag LayerTag, UEmberLayerBase* Layer)
 {
-	if (IsValid(OwningPlayer))
+	if (OwningPlayer)
 	{
 		if (AEmberMainHUD* MainHUD = Cast<AEmberMainHUD>(OwningPlayer->GetHUD()))
 		{
@@ -13,4 +15,42 @@ bool UUIFunctionLibrary::RegisterLayer(APlayerController* OwningPlayer, FGamepla
 	}
 	
 	return true;
+}
+
+UUserWidget* UUIFunctionLibrary::PushContentToLayer(APlayerController* OwningPlayer, const FGameplayTag LayerTag,
+	const TSubclassOf<UUserWidget>& WidgetClass)
+{
+	if (OwningPlayer)
+	{
+		return Cast<AEmberMainHUD>(OwningPlayer->GetHUD())->PushContentToLayer(LayerTag, WidgetClass);
+	}
+
+	return nullptr;
+}
+
+void UUIFunctionLibrary::PopContentToLayer(APlayerController* OwningPlayer, const FGameplayTag LayerTag)
+{
+	if (OwningPlayer)
+	{
+		Cast<AEmberMainHUD>(OwningPlayer->GetHUD())->PopContentToLayer(LayerTag);
+	}
+}
+
+void UUIFunctionLibrary::ClearToLayer(APlayerController* OwningPlayer, const FGameplayTag LayerTag)
+{
+	if (OwningPlayer)
+	{
+		Cast<AEmberMainHUD>(OwningPlayer->GetHUD())->ClearToLayer(LayerTag);
+	}
+}
+
+void UUIFunctionLibrary::FocusGame(APlayerController* OwningPlayer)
+{
+	if (OwningPlayer)
+	{
+		OwningPlayer->bShowMouseCursor = false;
+		OwningPlayer->SetInputMode(FInputModeGameOnly());
+		UWidgetBlueprintLibrary::SetFocusToGameViewport();
+		UGameplayStatics::SetGamePaused(OwningPlayer->GetWorld(), false);
+	}
 }
