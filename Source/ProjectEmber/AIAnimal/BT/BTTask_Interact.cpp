b9805b549c74ce2cffd183450eeb151fab35ea60
Jihyeon0 <wjswlqhd1@naver.com>
2025-05-16 14:10:38 +0900

    Commit: 어빌리티 적용 코드로 수정, 몽타주 설정은 아직

--- Source/ProjectEmber/AIAnimal/BT/BTTask_Interact.cpp
+++ Source/ProjectEmber/AIAnimal/BT/BTTask_Interact.cpp
@@ -4,6 +4,8 @@
 #include "AIAnimal/TestFood.h"
 #include "GameFramework/Character.h"
 //#include "Interactables/BaseInteractableActor.h"
+#include "AbilitySystemComponent.h"
+#include "Abilities/GameplayAbilityTypes.h"
 #include "Interactables/Interactable.h"
 #include "ProjectEmber/AIAnimal/BaseAIAnimal.h"
 
@@ -51,12 +53,25 @@ EBTNodeResult::Type UBTTask_Interact::ExecuteTask(UBehaviorTreeComponent& OwnerC
 		{
 			return EBTNodeResult::Failed;
 		}
+
+		//어빌리티에서 몽타주 재생 끝나는 시점에 바인딩된 함수 OnCompleteCallback 에서 처리하려고 했으나
+		//모듈 깨짐 -> 직접 몽타주에 노티파이 만들어서 처리해야할 듯
+		//AICharacter->SetFullness();
+		//BlackboardComp->SetValueAsBool("IsHungry", false);
+		//IInteractable::Execute_Interact(TargetActor,AICharacter); //먹이 삭제, testfood의 bp에 작성된 interact 호출
+		//BlackboardComp->SetValueAsObject(GetSelectedBlackboardKey(), nullptr);
 		
-		AICharacter->PlayInteractMontage(static_cast<uint8>(EAnimalAIState::Idle)); //enum을 넘기고는 있지만 아직 사용하지 않음
-		AICharacter->SetFullness();
-		BlackboardComp->SetValueAsBool("IsHungry", false);
-		Cast<ATestFood>(TargetActor)->Interact_Implementation(AICharacter); //먹이 삭제
-		BlackboardComp->SetValueAsObject(GetSelectedBlackboardKey(), nullptr);
+		//여기서는 어빌리티 호출만 해줌, 어빌리티 트리거 이벤트 -> 몽타주 재생 -> 재생되는 몽타주에 노티파이 -> 노티파이에서 실제로 먹는게 다 끝날 때, 중간에 끊길 때 경우 처리
+		FGameplayEventData Payload;
+		Payload.EventTag = InteractTaskTag;
+		Payload.Instigator = AICharacter;
+		Payload.Target = TargetActor;
+		Payload.OptionalObject = TargetActor;
+		Payload.OptionalObject2 = AICharacter->GetMontage();
+
+		// 게임플레이 이벤트를 어빌리티에게 전달(trigger)하는 함수, 어빌리티가 특정 GameplayTag 이벤트를 수신해서 발동되도록 하는 트리거 역할
+		AICharacter->GetAbilitySystemComponent()->HandleGameplayEvent(Payload.EventTag, &Payload);
+		
 		
 		// 태스크 성공 반환
 		return EBTNodeResult::Succeeded;
