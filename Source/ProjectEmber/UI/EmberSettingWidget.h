8e9a14c5535d329fac657d61b5044b1c4c09310c
GeonHo-Ko <rhrjsgh1@gmail.com>
2025-05-16 18:29:53 +0900

    Add Video Setting
    VideoSetting 추가 (해상도에 따른 메뉴 안보이는 문제 있음)(뒤로가기가 없음)

--- Source/ProjectEmber/UI/EmberSettingWidget.h
+++ Source/ProjectEmber/UI/EmberSettingWidget.h
@@ -4,10 +4,22 @@
 #include "Blueprint/UserWidget.h"
 #include "EmberSettingWidget.generated.h"
 
+class UButton;
+class UWidgetSwitcher;
+
 UCLASS()
 class PROJECTEMBER_API UEmberSettingWidget : public UUserWidget
 {
     GENERATED_BODY()
 protected:
     virtual void NativeConstruct() override;
+
+    UFUNCTION()
+    void OnVideoButtonClicked();
+
+    UPROPERTY(meta = (BindWidget))
+    UButton* VideoButton;
+
+    UPROPERTY(meta = (BindWidget))
+    UWidgetSwitcher* SettingsContentSwitcher;
 };
