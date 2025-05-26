b0b7fc679ea32dd2b987d3c118174c96418ed1c0
SeoWonHo <sjdlrnen0507@naver.com>
2025-05-26 16:08:44 +0900

    Add : ItemDetail 이름과 정보

---
+++ Source/ProjectEmber/Item/Core/EmberWidgetSlotData.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EmberWidgetSlotData.h"
+
+#include "EmberItemStruct.h"
+
+
+FEmberWidgetSlotData::FEmberWidgetSlotData(const FInstancedStruct& InSlotData)
+{
+	if (const FEmberSlotData* Slot = InSlotData.GetPtr<FEmberSlotData>())
+	{
+		UE_LOG(LogTemp, Display, TEXT("InventoryManagerComponent4: SlotData %d"),Slot->Quantity);
+		ItemID = Slot->ItemID;
+		Quantity = Slot->Quantity;
+		MaxStackSize = Slot->MaxStackSize;
+		ItemDisplayName = Slot->ItemDisplayName;
+		ItemDescription = Slot->ItemDescription;
+		if (Slot->SlotData)
+		{
+			if (const FSlotInfoRow* SlotInfoRow = Slot->SlotData->GetRow<FSlotInfoRow>("SlotInfo"))
+			{
+				ItemIcon = SlotInfoRow->ItemIcon;
+			}
+		}
+	}
+}
--- Source/ProjectEmber/Item/UI/SlotWidget/Slot/EmberBaseSlotWidget.h
+++ Source/ProjectEmber/Item/UI/SlotWidget/Slot/EmberBaseSlotWidget.h
@@ -6,6 +6,7 @@
 #include "GameplayTagContainer.h"
 #include "Blueprint/UserWidget.h"
 #include "Item/Core/EmberItemStruct.h"
+#include "Item/Core/EmberWidgetSlotData.h"
 #include "Item/UI/SlotWidget/EmberSlotDataProviderInterface.h"
 #include "EmberBaseSlotWidget.generated.h"
 
@@ -16,46 +17,6 @@ class UImage;
 /**
  * 
  */
-
-USTRUCT(BlueprintType)
-struct FWidgetSlotData
-{
-	GENERATED_BODY()
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Inventory Slot")
-	FName ItemID = NAME_None;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Inventory Slot", meta = (ClampMin = "0"))
-	int32 Quantity = 0;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Inventory Slot")
-	int32 MaxStackSize = 1;
-    
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ItemMasterInfoRow")
-	FText ItemDisplayName = FText();
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ItemMasterInfoRow")
-	FText ItemDescription = FText();
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ItemMasterInfoRow")
-	TSoftObjectPtr<UTexture2D> ItemIcon = nullptr;
-    
-	FWidgetSlotData() = default;
-
-	FWidgetSlotData(const FInstancedStruct& InSlotData);
-    
-	bool IsEmpty() const { return ItemID.IsNone() || Quantity <= 0; }
-    
-	void Clear()
-	{
-		ItemID = NAME_None;
-		Quantity = 0;
-		MaxStackSize = 1;
-		ItemDisplayName = FText();
-		ItemDescription = FText();
-		ItemIcon = nullptr;
-	}
-};
 
 UCLASS()
 class PROJECTEMBER_API UEmberBaseSlotWidget : public UUserWidget
@@ -75,14 +36,16 @@ public:
 	virtual void NativeOnDragDetected(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent, UDragDropOperation*& OutOperation) override;
 	virtual FReply NativeOnMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
 
-	virtual bool NativeOnDrop(const FGeometry& InGeometry, const FDragDropEvent& InDragDropEvent, UDragDropOperation* InOperation) override; 
+	virtual bool NativeOnDrop(const FGeometry& InGeometry, const FDragDropEvent& InDragDropEvent, UDragDropOperation* InOperation) override;
+	virtual void NativeOnMouseEnter(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
+	virtual void NativeOnMouseLeave(const FPointerEvent& InMouseEvent) override;
 public:
 	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Slot")
 	int32 SlotIndex = 0;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Slot")
-	FWidgetSlotData SlotData = FWidgetSlotData();
+	FEmberWidgetSlotData SlotData = FEmberWidgetSlotData();
 	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Slot")
 	FGameplayTag SlotType;
@@ -97,7 +60,10 @@ public:
 	TSoftObjectPtr<UTexture2D> DefaultSlotTexture = nullptr;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Slot")
-	TSubclassOf<UItemDetailWidget> ItemDetailWidget = nullptr;
+	TSubclassOf<UItemDetailWidget> ItemDetailWidgetClass = nullptr;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Slot")
+	TObjectPtr<UItemDetailWidget> ItemDetailWidget = nullptr;
 	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	TScriptInterface<IEmberSlotDataProviderInterface> DataProvider = nullptr;
