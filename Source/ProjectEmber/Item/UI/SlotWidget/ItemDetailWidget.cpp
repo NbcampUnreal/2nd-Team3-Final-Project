4bb272520889e2d25062b051c4af183adc4db8f3
SeoWonHo <sjdlrnen0507@naver.com>
2025-05-26 16:08:44 +0900

    Add : ItemDetail 이름과 정보

--- Source/ProjectEmber/Item/UI/SlotWidget/ItemDetailWidget.cpp
+++ Source/ProjectEmber/Item/UI/SlotWidget/ItemDetailWidget.cpp
@@ -2,3 +2,31 @@
 
 
 #include "ItemDetailWidget.h"
+
+#include "Blueprint/WidgetLayoutLibrary.h"
+
+
+void UItemDetailWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	if (TObjectPtr<APlayerController> PlayerController = GetOwningPlayer())
+	{
+		FVector2D MousePosition;
+		UWidgetLayoutLibrary::GetMousePositionScaledByDPI(PlayerController, MousePosition.X, MousePosition.Y);
+		this->SetPositionInViewport(MousePosition, false);
+	
+	}
+}
+
+void UItemDetailWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+	Super::NativeTick(MyGeometry, InDeltaTime);
+
+	if (TObjectPtr<APlayerController> PlayerController = GetOwningPlayer())
+	{
+		FVector2D MousePosition;
+		UWidgetLayoutLibrary::GetMousePositionScaledByDPI(PlayerController, MousePosition.X, MousePosition.Y);
+		this->SetPositionInViewport(MousePosition, false);
+	
+	}
+}
