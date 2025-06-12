b714fa2abb34b01fd75c5b53b28e8807405bcfe8
asdfg7936 <swa0708@gmail.com>
2025-06-12 17:28:20 +0900

    Save Load build

--- Source/ProjectEmber/ProjectEmber.Build.cs
+++ Source/ProjectEmber/ProjectEmber.Build.cs
@@ -34,7 +34,8 @@ public class ProjectEmber : ModuleRules
             "Slate",
             "SlateCore",
             "MotionWarping",
-            "TargetSystem"
+            "TargetSystem",
+            "EasyMultiSave"
         });
 
         //───────────────────────────────────────────────────────────────────────────────
